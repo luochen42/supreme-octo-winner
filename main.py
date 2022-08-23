6338422eec45a945473a678edb5336ebc4578b0c
luochen42 <83274907+luochen42@users.noreply.github.com>
2022-08-24 01:16:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -60,7 +60,6 @@ def get_best(city):
     return  tem_day
 
 client = WeChatClient(app_id, app_secret)
-
 wm = WeChatMessage(client)
 wea, temperature = get_weather()
 data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
